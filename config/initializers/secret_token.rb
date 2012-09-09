c824b2999345f7120f824464185ad8f4a276911a
ObaidMalik <obaid_malik@external.mckinsey.com>
2012-09-09 15:12:27 +0530

    Base Install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chaptwo::Application.config.secret_token = '8d3b82cf01fdbc391646f36d9178eab810fcf73e7b3548594722197939e6d7cb766d5c60f188815d10eb1ebf926bb54393ff52c81436994ce86578d7793fcbe0'
